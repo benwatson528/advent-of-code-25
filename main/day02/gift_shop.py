a3eb3c28d189afb2b3dcfaa32301ee0d6c042f1b
Ben Watson <benwatson528@gmail.com>
2025-12-02 05:56:19 +0000

    Day 2 part 2

--- main/day02/gift_shop.py
+++ main/day02/gift_shop.py
@@ -1,6 +1,20 @@
-def solve(ranges) -> int:
+import itertools
+
+
+def solve_p1(ranges) -> int:
     invalid_ids = []
     for s in [str(i) for r in ranges for i in range(r[0], r[1] + 1)]:
         if s[:len(s) // 2] == s[len(s) // 2:]:
             invalid_ids.append(s)
+    return sum(int(x) for x in invalid_ids)
+
+
+def solve_p2(ranges) -> int:
+    invalid_ids = []
+    for s in [str(i) for r in ranges for i in range(r[0], r[1] + 1)]:
+        for n in range(1, len(s) // 2 + 1):
+            if len(s) % n == 0:
+                if all(s[:n] == b for b in ["".join(x) for x in itertools.batched(s, n)]):
+                    invalid_ids.append(s)
+                    break
     return sum(int(x) for x in invalid_ids)
--- tests/day02/test_gift_shop.py
+++ tests/day02/test_gift_shop.py
@@ -1,15 +1,23 @@
 import os
 from pathlib import Path
 
-from main.day02.gift_shop import solve
+from main.day02.gift_shop import solve_p1, solve_p2
 
 
 def test_p1_simple():
-    assert solve(read_input("data/test_input.txt")) == 1227775554
+    assert solve_p1(read_input("data/test_input.txt")) == 1227775554
 
 
 def test_p1_real():
-    assert solve(read_input("data/input.txt")) == 19574776074
+    assert solve_p1(read_input("data/input.txt")) == 19574776074
+
+
+def test_p2_simple():
+    assert solve_p2(read_input("data/test_input.txt")) == 4174379265
+
+
+def test_p2_real():
+    assert solve_p2(read_input("data/input.txt")) == 25912654282
 
 
 def read_input(file_name):

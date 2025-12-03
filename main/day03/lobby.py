0d7f1f002c4cc683fcafccd0040b3ca52bb404c2
Ben Watson <benwatson528@gmail.com>
2025-12-03 06:30:04 +0000

    Day 3 part 2 partial

--- main/day03/lobby.py
+++ main/day03/lobby.py
@@ -1,9 +1,19 @@
-def solve(banks) -> int:
+def solve_p1(banks) -> int:
     joltage = 0
     for b in banks:
-        biggest = sorted(b, reverse=True)[0]
-        if b.index(biggest) == len(b) - 1:
-            biggest = sorted(b, reverse=True)[1]
+        biggest = sorted(b[:-1], reverse=True)[0]
         right_max = max(b[b.index(biggest) + 1:])
         joltage += int(biggest + right_max)
+    return joltage
+
+
+def find_biggest_rec(b, n, j):
+    return find_biggest_rec(b, n - 1, j) if n > 0 else j
+
+
+def solve_p2(banks) -> int:
+    joltage = 0
+    for b in banks:
+        biggest = find_biggest_rec(b, 12, "")
+        joltage += int(biggest)
     return joltage
--- tests/day03/test_lobby.py
+++ tests/day03/test_lobby.py
@@ -1,15 +1,23 @@
 import os
 from pathlib import Path
 
-from main.day03.lobby import solve
+from main.day03.lobby import solve_p1, solve_p2
 
 
 def test_p1_simple():
-    assert solve(read_input("data/test_input.txt")) == 357
+    assert solve_p1(read_input("data/test_input.txt")) == 357
 
 
 def test_p1_real():
-    assert solve(read_input("data/input.txt")) == 17408
+    assert solve_p1(read_input("data/input.txt")) == 17408
+
+
+def test_p2_simple():
+    assert solve_p2(read_input("data/test_input.txt")) == 3121910778619
+
+
+def test_p2_real():
+    assert solve_p2(read_input("data/input.txt")) == 17408
 
 
 def read_input(file_name):

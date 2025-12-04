e5ffb61585ef2d4cb9cfd4b8db282ea444237190
Ben Watson <77007947+benwatsonnandos@users.noreply.github.com>
2025-12-04 07:48:46 +0000

    Day 4 part 2

--- main/day04/printing_department.py
+++ main/day04/printing_department.py
@@ -1,5 +1,16 @@
 ADJACENT = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, -1), (-1, 1), (-1, -1), (1, 1)]
 
 
-def solve(rolls) -> int:
-    return sum(sum((r[0] + d[0], r[1] + d[1]) in rolls for d in ADJACENT) < 4 for r in rolls)
+def find_num_adjacent(r, rolls) -> int: return sum((r[0] + d[0], r[1] + d[1]) in rolls for d in ADJACENT)
+
+
+def solve_p1(rolls) -> int:
+    return sum(find_num_adjacent(r, rolls) < 4 for r in rolls)
+
+
+def solve_p2(rolls) -> int:
+    total_removed = 0
+    while to_remove := {r for r in rolls if find_num_adjacent(r, rolls) < 4}:
+        total_removed += len(to_remove)
+        rolls -= to_remove
+    return total_removed
--- tests/day04/test_printing_department.py
+++ tests/day04/test_printing_department.py
@@ -1,15 +1,23 @@
 import os
 from pathlib import Path
 
-from main.day04.printing_department import solve
+from main.day04.printing_department import solve_p1, solve_p2
 
 
 def test_p1_simple():
-    assert solve(read_input("data/test_input.txt")) == 13
+    assert solve_p1(read_input("data/test_input.txt")) == 13
 
 
 def test_p1_real():
-    assert solve(read_input("data/input.txt")) == 1370
+    assert solve_p1(read_input("data/input.txt")) == 1370
+
+
+def test_p2_simple():
+    assert solve_p2(read_input("data/test_input.txt")) == 43
+
+
+def test_p2_real():
+    assert solve_p2(read_input("data/input.txt")) == 8437
 
 
 def read_input(file_name):

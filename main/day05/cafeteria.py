bdc0dc0ed603c07b2eda540049f547a5d67095de
Ben Watson <benwatson528@gmail.com>
2025-12-05 12:38:30 +0000

    Day 5 part 2

--- main/day05/cafeteria.py
+++ main/day05/cafeteria.py
@@ -1,2 +1,19 @@
-def solve(ranges, available) -> int:
-    return sum(any(x in r for r in ranges) for x in available)
+def solve_p1(ranges, available) -> int:
+    return sum(any(x in range(r.start, r.stop + 1) for r in ranges) for x in available)
+
+
+def solve_p2(ranges) -> int:
+    final_ranges = set()
+    sorted_ranges = sorted(ranges, key=lambda x: x.start)
+    low, high = sorted_ranges[0].start, sorted_ranges[0].stop
+    for r in sorted_ranges[1:]:
+        # overlap found, expand the end and keep going
+        if r.start <= high < r.stop:
+            high = r.stop
+        # no overlap found, close the existing window and start a new one
+        elif not (r.start <= high >= r.stop):
+            final_ranges.add((low, high))
+            low, high = r.start, r.stop
+    if high > max(r[1] for r in final_ranges):
+        final_ranges.add((low, high))
+    return sum(r[1] + 1 - r[0] for r in final_ranges)
--- tests/day05/test_cafeteria.py
+++ tests/day05/test_cafeteria.py
@@ -1,15 +1,23 @@
 import os
 from pathlib import Path
 
-from main.day05.cafeteria import solve
+from main.day05.cafeteria import solve_p1, solve_p2
 
 
 def test_p1_simple():
-    assert solve(*read_input("data/test_input.txt")) == 3
+    assert solve_p1(*read_input("data/test_input.txt")) == 3
 
 
 def test_p1_real():
-    assert solve(*read_input("data/input.txt")) == 896
+    assert solve_p1(*read_input("data/input.txt")) == 896
+
+
+def test_p2_simple():
+    assert solve_p2(read_input("data/test_input.txt")[0]) == 14
+
+
+def test_p2_real():
+    assert solve_p2(read_input("data/input.txt")[0]) == 346240317247002
 
 
 def read_input(file_name):
@@ -22,7 +30,7 @@ def read_input(file_name):
                 is_ranges = False
                 continue
             if is_ranges:
-                ranges.append(range(int(l.split("-")[0]), int(l.split("-")[1]) + 1))
+                ranges.append(range(int(l.split("-")[0]), int(l.split("-")[1])))
             else:
                 available.append(int(l))
     return ranges, available

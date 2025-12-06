54a28d1997b148c3356f356dce49bfc260dd9465
Ben Watson <benwatson528@gmail.com>
2025-12-06 08:12:43 +0000

    Day 6 part 2

--- main/day06/trash_compactor.py
+++ main/day06/trash_compactor.py
@@ -1,2 +1,25 @@
-def solve(ls) -> int:
+def solve_p1(ls) -> int:
     return sum(eval(l[-1].join(l[:-1])) for l in ls)
+
+
+def solve_p2(ls) -> int:
+    answers = []
+    equation = []
+    operator = None
+    for col in range(len(ls[0])):
+        row = ""
+        for j in range(len(ls)):
+            c = ls[j][col]
+            if c in ("+", "*"):
+                operator = c
+            else:
+                row += c
+        if not row.strip():
+            answers.append(eval(operator.join(equation)))
+            equation = []
+            operator = None
+        if row.strip():
+            equation.append(row.strip())
+        print()
+    answers.append(eval(operator.join(equation)))
+    return sum(answers)
--- tests/day06/test_trash_compactor.py
+++ tests/day06/test_trash_compactor.py
@@ -1,19 +1,26 @@
 import os
-
 from pathlib import Path
 
-from main.day06.trash_compactor import solve
+from main.day06.trash_compactor import solve_p1, solve_p2
 
 
 def test_p1_simple():
-    assert solve(read_input("data/test_input.txt")) == 4277556
+    assert solve_p1(read_input_p1("data/test_input.txt")) == 4277556
 
 
 def test_p1_real():
-    assert solve(read_input("data/input.txt")) == 6343365546996
+    assert solve_p1(read_input_p1("data/input.txt")) == 6343365546996
 
 
-def read_input(file_name):
+def test_p2_simple():
+    assert solve_p2(read_input_p2("data/test_input.txt")) == 3263827
+
+
+def test_p2_real():
+    assert solve_p2(read_input_p2("data/input.txt")) == 11136895955912
+
+
+def read_input_p1(file_name):
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
         rows = []
         lines = f.read().splitlines()
@@ -23,3 +30,14 @@ def read_input(file_name):
                     rows.append([])
                 rows[i].append(b)
         return rows
+
+
+def read_input_p2(file_name):
+    with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
+        cols = []
+        lines = f.read().splitlines()
+        for j, a in enumerate(lines):
+            cols.append([])
+            for i, b in enumerate(reversed(lines[j])):
+                cols[j].append(b)
+        return cols

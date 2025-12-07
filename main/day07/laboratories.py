8932e2b264fdb69d7d268c999509dc38bb6bf5d0
Ben Watson <benwatson528@gmail.com>
2025-12-07 07:31:08 +0000

    Day 7 part 2

--- main/day07/laboratories.py
+++ main/day07/laboratories.py
@@ -1,11 +1,12 @@
-def solve(splitters, start) -> int:
+from functools import lru_cache
+
+
+def solve_p1(splitters, start) -> int:
     num_splitter_hits = 0
     beam_tips = [start]
-    # we know we go down 1 each turn even if it hits a splitter
     for _ in range(max(y for _, y in splitters)):
         new_beam_tips = set()
         for beam in beam_tips:
-            # either move active splitters down or split them
             if (beam[0], beam[1] + 1) in splitters:
                 num_splitter_hits += 1
                 new_beam_tips.add((beam[0] - 1, beam[1] + 1))
@@ -14,3 +15,18 @@ def solve(splitters, start) -> int:
                 new_beam_tips.add((beam[0], beam[1] + 1))
         beam_tips = new_beam_tips
     return num_splitter_hits
+
+
+def solve_p2(splitters, start) -> int:
+    return move(start, frozenset(splitters), max(y for _, y in splitters)) + 1
+
+
+@lru_cache
+def move(beam, splitters, max_depth) -> int:
+    if beam[1] == max_depth:
+        return 0
+    elif (beam[0], beam[1] + 1) in splitters:
+        return move((beam[0] - 1, beam[1] + 1), splitters, max_depth) + move(
+            (beam[0] + 1, beam[1] + 1), splitters, max_depth) + 1
+    else:
+        return move((beam[0], beam[1] + 1), splitters, max_depth)
--- tests/day07/test_laboratories.py
+++ tests/day07/test_laboratories.py
@@ -1,15 +1,23 @@
 import os
 from pathlib import Path
 
-from main.day07.laboratories import solve
+from main.day07.laboratories import solve_p1, solve_p2
 
 
 def test_p1_simple():
-    assert solve(*read_input("data/test_input.txt")) == 21
+    assert solve_p1(*read_input("data/test_input.txt")) == 21
 
 
 def test_p1_real():
-    assert solve(*read_input("data/input.txt")) == 1602
+    assert solve_p1(*read_input("data/input.txt")) == 1602
+
+
+def test_p2_simple():
+    assert solve_p2(*read_input("data/test_input.txt")) == 40
+
+
+def test_p2_real():
+    assert solve_p2(*read_input("data/input.txt")) == 135656430050438
 
 
 def read_input(file_name):

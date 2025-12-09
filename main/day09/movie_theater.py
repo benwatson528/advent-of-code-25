4350346197c3258d8d787bd8894ec6cc4f5cd035
Ben Watson <benwatson528@gmail.com>
2025-12-09 21:12:56 +0000

    Day 9 both parts

--- main/day09/movie_theater.py
+++ main/day09/movie_theater.py
@@ -1,8 +1,22 @@
 import itertools
 
+from shapely.geometry.polygon import Polygon
 
-def solve(red_tiles) -> int:
+
+def solve_p1(red_tiles) -> int:
     return sorted(
         [((p1, p2), (abs(p1[0] - p2[0]) + 1) * (abs(p1[1] - p2[1]) + 1)) for p1, p2 in
          itertools.combinations(red_tiles, 2)],
         key=lambda x: -x[1])[0][1]
+
+
+def solve_p2(red_tiles) -> int:
+    polygon = Polygon(red_tiles)
+    max_area = 0
+    for c1, c2 in itertools.combinations(red_tiles, 2):
+        min_x, max_x = min(c1[0], c2[0]), max(c1[0], c2[0])
+        min_y, max_y = min(c1[1], c2[1]), max(c1[1], c2[1])
+        rectangle = Polygon([(min_x, min_y), (max_x, min_y), (max_x, max_y), (min_x, max_y)])
+        if polygon.contains(rectangle):
+            max_area = max(max_area, ((max_x - min_x) + 1) * ((max_y - min_y) + 1))
+    return max_area
--- tests/day09/test_movie_theater.py
+++ tests/day09/test_movie_theater.py
@@ -1,18 +1,25 @@
 import os
 from pathlib import Path
 
-from main.day09.movie_theater import solve
+from main.day09.movie_theater import solve_p1, solve_p2
 
 
 def test_p1_simple():
-    assert solve(read_input("data/test_input.txt")) == 50
+    assert solve_p1(read_input("data/test_input.txt")) == 50
 
 
 def test_p1_real():
-    assert solve(read_input("data/input.txt")) == 4733727792
+    assert solve_p1(read_input("data/input.txt")) == 4733727792
+
+
+def test_p2_simple():
+    assert solve_p2(read_input("data/test_input.txt")) == 24
+
+
+def test_p2_real():
+    assert solve_p2(read_input("data/input.txt")) == 1566346198
 
 
 def read_input(file_name):
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
         return [(int(l.split(",")[0]), int(l.split(",")[1])) for l in f.read().splitlines()]
-

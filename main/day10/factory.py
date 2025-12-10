0a19d7fd7f6b5992f5e6037842fe93825372336d
Ben Watson <77007947+benwatsonnandos@users.noreply.github.com>
2025-12-10 17:41:44 +0000

    Day 10 part 2 not working

--- main/day10/factory.py
+++ main/day10/factory.py
@@ -1,4 +1,84 @@
-def solve(manual) -> int:
-    presses = 0
-    
-    return sum(presses)
+from collections import deque, defaultdict
+from string import ascii_lowercase
+
+from z3 import Int, Solver
+
+
+def solve_p1(manual) -> int:
+    return sum(toggle_p1(m[0], m[1]) for m in manual)
+
+
+def solve_p2(manual) -> int:
+    num_turns = 0
+    for m in manual:
+        buttons = m[1]
+        components = defaultdict(list)
+        joltages = [int(x) for x in m[2].split(",")]
+        for j in range(len(joltages)):
+            components[j] = []
+        for i, buttons in enumerate(m[1]):
+            for b in buttons:
+                components[int(b)].append(ascii_lowercase[i])
+        num_turns += solve_equations(components, joltages)
+    return num_turns
+
+
+def solve_equations(components, joltages):
+    a = Int('a')
+    b = Int('b')
+    c = Int('c')
+    d = Int('d')
+    e = Int('e')
+    f = Int('f')
+    g = Int('g')
+    h = Int('h')
+    i = Int('i')
+    j = Int('j')
+    k = Int('k')
+    l = Int('l')
+    m = Int('m')
+    n = Int('n')
+    o = Int('o')
+    p = Int('p')
+    q = Int('q')
+    r = Int('r')
+    s = Int('s')
+    t = Int('t')
+    u = Int('u')
+    v = Int('v')
+    w = Int('w')
+    x = Int('x')
+    y = Int('y')
+    z = Int('z')
+
+    # equations = []
+    solver = Solver()
+    for ascii_char in ascii_lowercase[:26]:
+        solver.add(eval(f"{ascii_char} >= 0"))
+    for comp, ls in components.items():
+        solver.add(eval(f"{" + ".join(ls)} == {joltages[comp]}"))
+    check = solver.check()
+    model = solver.model()
+    ans = 0
+    for model_result in model:
+        ans += int(model[model_result].as_string())
+    return ans  # return sum(int(model[model_result]) for model_result in model)
+
+
+def toggle_p1(expected_indicator, buttons):
+    q = deque()
+    seen = set()
+    q.append((["."] * len(expected_indicator), 0))
+    while q:
+        q = deque(sorted(q, key=lambda x: x[1]))
+        indicator, num_presses = q.popleft()
+        for button in buttons:
+            copied_indicator = indicator.copy()
+            for b in button:
+                copied_indicator[int(b)] = "." if copied_indicator[int(b)] == "#" else "#"
+            if copied_indicator == expected_indicator:
+                return num_presses + 1
+            if "".join(copied_indicator) not in seen:
+                q.append((copied_indicator, num_presses + 1))
+            seen.add("".join(copied_indicator))
+    return -1
--- tests/day10/test_factory.py
+++ tests/day10/test_factory.py
@@ -1,23 +1,35 @@
 import os
 from pathlib import Path
 
-from main.day10.factory import solve
+from main.day10.factory import solve_p1, solve_p2
 
 
 def test_p1_simple():
-    assert solve(read_input("data/test_input.txt")) == 7
+    assert solve_p1(read_input("data/test_input.txt")) == 7
 
 
 def test_p1_real():
-    assert solve(read_input("data/input.txt")) == 0
+    assert solve_p1(read_input("data/input.txt")) == 512
+
+
+def test_p2_simple():
+    assert solve_p2(read_input("data/test_input.txt")) == 33
+
+
+def test_p2_real():
+    assert solve_p2(read_input("data/input.txt")) == 20415
+    # 20463 too high
+    # 20461 too high
+    # 20415 too high
+
 
 
 def read_input(file_name):
     manual = []
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
         for l in f.read().splitlines():
-            indicator = l.split("]")[0][1:]
-            button = [x.split(",") for x in l.split("] ")[1].split(" {")[0].replace("(", "").replace(")", "").split()]
-            joltage = l.split("{")[1][:-1]
-            manual.append((indicator, button, joltage))
+            indicators = list(l.split("]")[0][1:])
+            buttons = [x.split(",") for x in l.split("] ")[1].split(" {")[0].replace("(", "").replace(")", "").split()]
+            joltages = l.split("{")[1][:-1]
+            manual.append((indicators, buttons, joltages))
     return manual

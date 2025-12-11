3a47c7efa20cac8ba2aaaf9986ad24c293f0739a
Ben Watson <benwatson528@gmail.com>
2025-12-11 08:06:49 +0000

    Day 11 both parts

--- main/day11/reactor.py
+++ main/day11/reactor.py
@@ -1,14 +1,11 @@
 from collections import deque
+from functools import cache
 
 
-def solve(devices) -> int:
-    return find_outs(devices, "you")
-
-
-def find_outs(devices, start):
+def solve_p1(devices) -> int:
     num_paths = 0
     q = deque()
-    q.append((start, set()))
+    q.append(("you", set()))
     while q:
         n, visited = q.popleft()
         if n == "out":
@@ -17,3 +14,22 @@ def find_outs(devices, start):
         for out in devices[n]:
             q.append((out, visited | {n}))
     return num_paths
+
+
+def solve_p2(devices) -> int:
+    global DEVICES
+    DEVICES = devices
+    return traverse_rec("svr", False, False)
+
+
+@cache
+def traverse_rec(current, seen_dac, seen_fft):
+    if current == "out":
+        return 1 if seen_dac and seen_fft else 0
+    else:
+        ans = 0
+        for output in DEVICES[current]:
+            new_dac = seen_dac or output == "dac"
+            new_fft = seen_fft or output == "fft"
+            ans += traverse_rec(output, new_dac, new_fft)
+        return ans
--- tests/day11/test_reactor.py
+++ tests/day11/test_reactor.py
@@ -2,15 +2,19 @@ import os
 from collections import defaultdict
 from pathlib import Path
 
-from main.day11.reactor import solve
+from main.day11.reactor import solve_p1, solve_p2
 
 
 def test_p1_simple():
-    assert solve(read_input("data/test_input.txt")) == 5
+    assert solve_p1(read_input("data/test_input_p1.txt")) == 5
 
 
 def test_p1_real():
-    assert solve(read_input("data/input.txt")) == 511
+    assert solve_p1(read_input("data/input.txt")) == 511
+
+
+def test_p2_real():
+    assert solve_p2(read_input("data/input.txt")) == 458618114529380
 
 
 def read_input(file_name):
